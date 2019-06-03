05e1ab7af7519da50bc412e6831409e26bc06915
kainanpr <kainanpr95@gmail.com>
2019-06-02 23:11:36 -0300

    Adicionando atributo qtdEstoque ao Alimento

--- src/model/Alimento.java
+++ src/model/Alimento.java
@@ -7,23 +7,29 @@ public class Alimento {
     private int id;
     private String nomeAlimento;
     private LocalDate dataValidade;
+    private int qtdEstoque;
     private String tipo;
+    private Usuario usuario;
 
     public Alimento() {
 
     }
 
-    public Alimento(int id, String nomeAlimento, LocalDate dataValidade, String tipo) {
+    public Alimento(int id, String nomeAlimento, LocalDate dataValidade, int qtdEstoque, String tipo, Usuario usuario) {
         this.id = id;
         this.nomeAlimento = nomeAlimento;
         this.dataValidade = dataValidade;
+        this.qtdEstoque = qtdEstoque;
         this.tipo = tipo;
+        this.usuario = usuario;
     }
 
-    public Alimento(String nomeAlimento, LocalDate dataValidade, String tipo) {
+    public Alimento(String nomeAlimento, LocalDate dataValidade, int qtdEstoque, String tipo, Usuario usuario) {
         this.nomeAlimento = nomeAlimento;
         this.dataValidade = dataValidade;
+        this.qtdEstoque = qtdEstoque;
         this.tipo = tipo;
+        this.usuario = usuario;
     }
 
     public int getId() {
@@ -50,6 +56,14 @@ public class Alimento {
         this.dataValidade = dataValidade;
     }
 
+    public int getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public void setQtdEstoque(int qtdEstoque) {
+        this.qtdEstoque = qtdEstoque;
+    }
+
     public String getTipo() {
         return tipo;
     }
@@ -58,13 +72,23 @@ public class Alimento {
         this.tipo = tipo;
     }
 
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
     @Override
     public String toString() {
         return "Alimento{" +
                 "id=" + id +
                 ", nomeAlimento='" + nomeAlimento + '\'' +
                 ", dataValidade=" + dataValidade +
+                ", qtdEstoque=" + qtdEstoque +
                 ", tipo='" + tipo + '\'' +
+                ", usuario=" + usuario +
                 '}';
     }
 }
--- src/persistence/dao/AlimentoDao.java
+++ src/persistence/dao/AlimentoDao.java
@@ -1,6 +1,8 @@
 package persistence.dao;
 
 import model.Alimento;
+import model.Permissao;
+import model.Usuario;
 import persistence.connection.ConexaoDb;
 
 import java.sql.*;
@@ -16,12 +18,14 @@ public class AlimentoDao {
 
         try {
             conn = ConexaoDb.getConnection();
-            String sql = "INSERT INTO alimento (nome_alimento, data_validade, tipo) " +
-                    "VALUES(?, ?, ?)";
+            String sql = "INSERT INTO alimento (nome_alimento, data_validade, qtd_estoque, tipo, id_usuario) " +
+                    "VALUES(?, ?, ?, ? ,?)";
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, alimento.getNomeAlimento());
             ps.setDate(2, Date.valueOf(alimento.getDataValidade()));
-            ps.setString(3, alimento.getTipo());
+            ps.setInt(3, alimento.getQtdEstoque());
+            ps.setString(4, alimento.getTipo());
+            ps.setInt(5, alimento.getUsuario().getId());
             ps.executeUpdate();
             ps.close();
             conn.close();
@@ -34,13 +38,14 @@ public class AlimentoDao {
 
         try {
             conn = ConexaoDb.getConnection();
-            String sql = "UPDATE alimento SET nome_alimento = ?, data_validade = ?, tipo = ?" +
+            String sql = "UPDATE alimento SET nome_alimento = ?, data_validade = ?, qtd_estoque = ?, tipo = ?" +
                     " WHERE id_alimento = ?";
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, alimento.getNomeAlimento());
             ps.setDate(2, Date.valueOf(alimento.getDataValidade()));
-            ps.setString(3, alimento.getTipo());
-            ps.setInt(4, alimento.getId());
+            ps.setInt(3, alimento.getQtdEstoque());
+            ps.setString(4, alimento.getTipo());
+            ps.setInt(5, alimento.getId());
             ps.executeUpdate();
             ps.close();
             conn.close();
@@ -54,17 +59,28 @@ public class AlimentoDao {
 
         try {
             conn = ConexaoDb.getConnection();
-            String sql = "SELECT * FROM alimento WHERE LOWER(tipo) LIKE ?";
+            String sql = "SELECT * FROM alimento a INNER JOIN usuario u ON a.id_usuario = u.id WHERE LOWER(tipo) LIKE ?";
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, "%" + tipo + "%");
             ResultSet rs = ps.executeQuery();
             List<Alimento> listAlimentos = new ArrayList<>();
 
             while (rs.next()) {
+                Usuario usuario = new Usuario(rs.getInt(7),
+                        rs.getString(8),
+                        rs.getString(9),
+                        rs.getString(10),
+                        rs.getString(11),
+                        rs.getString(12),
+                        rs.getString(13),
+                        Permissao.valueOf(rs.getString(14)));
+
                 Alimento alimento = new Alimento(rs.getInt(1),
                         rs.getString(2),
                         rs.getDate(3).toLocalDate(),
-                        rs.getString(4));
+                        rs.getInt(4),
+                        rs.getString(5),
+                        usuario);
                 listAlimentos.add(alimento);
             }
 
@@ -83,17 +99,27 @@ public class AlimentoDao {
 
         try {
             conn = ConexaoDb.getConnection();
-            String sql = "SELECT * FROM alimento ORDER BY nome_alimento DESC";
+            String sql = "SELECT * FROM alimento a INNER JOIN usuario u ON a.id_usuario = u.id ORDER BY nome_alimento DESC";
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             List<Alimento> listAlimentos = new ArrayList<>();
 
             while (rs.next()) {
-                Alimento alimento = new Alimento(
-                        rs.getInt(1),
+                Usuario usuario = new Usuario(rs.getInt(7),
+                        rs.getString(8),
+                        rs.getString(9),
+                        rs.getString(10),
+                        rs.getString(11),
+                        rs.getString(12),
+                        rs.getString(13),
+                        Permissao.valueOf(rs.getString(14)));
+
+                Alimento alimento = new Alimento(rs.getInt(1),
                         rs.getString(2),
                         rs.getDate(3).toLocalDate(),
-                        rs.getString(4));
+                        rs.getInt(4),
+                        rs.getString(5),
+                        usuario);
                 listAlimentos.add(alimento);
             }
 
