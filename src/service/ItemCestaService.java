97b417fdbaef135c40d667d8912b2182ecf534e6
kainanpr <kainanpr95@gmail.com>
2019-06-24 00:37:43 -0300

    Atualizando estoque ao criar cesta

--- src/service/ItemCestaService.java
+++ src/service/ItemCestaService.java
@@ -1,17 +1,32 @@
 package service;
 
+import model.Alimento;
 import model.ItemCesta;
+import persistence.dao.AlimentoDao;
 import persistence.dao.ItemCestaDao;
 
 import java.util.List;
 
 public class ItemCestaService {
     private ItemCestaDao itemCestaDao = new ItemCestaDao();
+    private AlimentoDao alimentoDao = new AlimentoDao();
 
     public void inserir(List<ItemCesta> listaItemCesta) {
         for (ItemCesta item : listaItemCesta) {
             itemCestaDao.inserir(item);
+            atualizarEstoque(item);
         }
+    }
+
+    private void atualizarEstoque(ItemCesta itemCesta) {
+        Alimento alimento = itemCesta.getAlimento();
+
+        final int qtdEstoque = alimento.getQtdEstoque();
+        final int qtdDoados = itemCesta.getQuantidade();
+
+        alimento.setQtdEstoque(qtdEstoque - qtdDoados);
+
+        alimentoDao.atualizar(alimento);
     }
 
     public List<ItemCesta> listarPorIdDaCesta(int id) {
